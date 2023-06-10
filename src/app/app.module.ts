ae456deada72ca71c3e491015addeb2a98f9d951
Ricardo <ric.asjunior@gmail.com>
2023-06-10 19:25:11 -0300

    Criando telas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaLivrosComponent } from './components/lista-livros/lista-livros.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaLivrosComponent
+    ListaLivrosComponent,
+    FormularioComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/lista-livros/lista-livros.component.ts
+++ src/app/components/lista-livros/lista-livros.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista-livros.component.css']
 })
 export class ListaLivrosComponent {
-  name: string = "Ricardo"
+ 
 }
