6f7eeee63265f4101b0d0a5eb527826b6761ca98
Ricardo <ric.asjunior@gmail.com>
2023-06-10 15:37:03 -0300

    Iniciando projeto com Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaLivrosComponent } from './components/lista-livros/lista-livros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaLivrosComponent
   ],
   imports: [
     BrowserModule,
